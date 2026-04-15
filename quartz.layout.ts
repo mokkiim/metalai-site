7391076ef1432c9b9b32e19bb09445590f3842c1
김현국 <kimhyunkook@gimhyeongug-ui-MacBookPro-7.local>
2026-04-15 18:12:44 +0900

    fix: remove nav logo, replace article title with logo PNG on index

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,6 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [
-    Component.PageTitle(),
     Component.NavLinks(),
   ],
   afterBody: [Component.Newsletter()],
--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,9 +1,20 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
+import { pathToRoot, joinSegments } from "../util/path"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
   if (title) {
+    const isIndex = fileData.slug === "index"
+    if (isIndex) {
+      const baseDir = pathToRoot(fileData.slug!)
+      const logoPath = joinSegments(baseDir, "static/logo.png")
+      return (
+        <h1 class={classNames(displayClass, "article-title article-title-logo")}>
+          <img src={logoPath} alt={title} class="article-logo-img" />
+        </h1>
+      )
+    }
     return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
   } else {
     return null
@@ -13,6 +24,14 @@ const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzCompone
 ArticleTitle.css = `
 .article-title {
   margin: 2rem 0 0 0;
+}
+.article-title-logo {
+  text-align: left;
+}
+.article-logo-img {
+  height: 40px;
+  width: auto;
+  border-radius: 0 !important;
 }
 `
 
